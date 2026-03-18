f80f5d6f2a2a0b62e6862f67ce279bc9b17d1ab1
Maffin <maffinlp@yahoo.com>
2026-03-18 11:00:35 +0100

    Added option to only show unsubscribed

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,3 +1,4 @@
 MaffinLoader.Settings = MaffinLoader.Settings or {}
 
-MaffinLoader.Settings.DisplayWorkshopImages = false
+MaffinLoader.Settings.DisplayWorkshopImages = false -- Usually Id just show em but gahdamn are they bad for performance...
+MaffinLoader.Settings.HideSubscribedItems = true    -- Hides items from the downloadlist if they are already subscribed to.
--- lua/vgui/downloadentry.lua
+++ lua/vgui/downloadentry.lua
@@ -4,11 +4,25 @@ function PANEL:Init()
 end
 
 function PANEL:PerformLayout(width, height)
-    self.image:SetWidth(height-5)
+    if self.image then 
+        self.image:SetWidth(height-5)
+    end
 end
 
 function MaffinLoader.CreateDownloadEntry(imagePath, parent, fileInfo) -- fileInfo: https://gmodwiki.com/steamworks.FileInfo
-    local panel = vgui.Create("MaffinLoaderDownloadEntryHUD", parent)
+    local panel = parent:Add("MaffinLoaderDownloadEntryHUD")
+    local isSubscribed = steamworks.IsSubscribed(fileInfo.fileid)
+    local text = "Subscribe"
+    if isSubscribed then        
+        text = "Already Subscribed"
+        panel:SetVisible(not MaffinLoader.Settings.HideSubscribedItems)
+        local size = MaffinLoader.Settings.HideSubscribedItems and 0 or 64
+        panel:SetTall(size)
+        panel:GetParent():SetTall(size)
+    else
+        panel:SetTall(64)
+    end
+    panel:GetParent():InvalidateLayout(true)
     if MaffinLoader.Settings.DisplayWorkshopImages then
         local mat = AddonMaterial(imagePath)
 
@@ -26,7 +40,6 @@ function MaffinLoader.CreateDownloadEntry(imagePath, parent, fileInfo) -- fileIn
 
     panel.subscribeButton = vgui.Create("DButton", panel.middleSection)
     panel.subscribeButton:Dock(BOTTOM)
-    local text = steamworks.IsSubscribed(fileInfo.fileid) and "Already Subscribed" or "Subscribe" 
     panel.subscribeButton:SetText(text)
     panel.subscribeButton.DoClick = function()
         gui.OpenURL("https://steamcommunity.com/sharedfiles/filedetails/?id=" .. tostring(fileInfo.fileid))
